15a7d6afa94d1258b997cb52fd60c159af8f9b60
overpayyy <maksym.slipchyshyn@microsoft.wsei.edu.pl>
2026-01-09 18:18:30 +0100

    zadanie 2

---
+++ asp_net_mvc_lab1/Controllers/BlogController.cs
@@ -0,0 +1,24 @@
+﻿using aspNetMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspNetMVC.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly ILogger<BlogController> _logger;
+
+        public BlogController(ILogger<BlogController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new BlogViewModel()
+            {
+                Title = "Welcome to My Blog",
+                Description = "This is a simple blog application built with ASP.NET MVC."
+            });
+        }
+    }
+}
---
+++ asp_net_mvc_lab1/Models/BlogViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace aspNetMVC.Models
+{
+    public class BlogViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
